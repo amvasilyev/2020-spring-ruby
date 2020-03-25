2aec5a1d1e9b52e93ece58cfd59c471ef0567128
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2020-03-25 09:15:28 +0300

    Add template for the 3rd practice

---
+++ 2020-03-25-practice/comparing_tools.rb
@@ -0,0 +1,25 @@
+def main
+  array = input_data
+  solve_with_cycles(array)
+  solve_with_iterators(array)
+end
+
+def input_data
+  [12, 56, 77, 88]
+end
+
+def solve_with_cycles(array)
+  for number in array
+    puts number
+  end
+end
+
+def solve_with_iterators(array)
+  array.each do |number|
+    puts number
+  end
+end
+
+if __FILE__ == $0
+  main
+end

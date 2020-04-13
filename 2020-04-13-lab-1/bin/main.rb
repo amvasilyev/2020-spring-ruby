ecc8a7eaea5157f112d4a276d41f347e26d05ac6
Andrey <andrey.vasilyev@fruct.org>
2020-04-13 11:04:27 +0300

    Add menu for the application

---
+++ 2020-04-13-lab-1/bin/main.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative '../lib/menu'
+
+menu = Menu.new
+menu.show_menu
---
+++ 2020-04-13-lab-1/lib/menu.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'tty-prompt'
+
+# The main class of our application
+class Menu
+  def initialize
+    @prompt = TTY::Prompt.new
+  end
+
+  MAIN_MENU_CHOICES = [
+    { name: 'Вывести расписание движения поездов', value: :rasp },
+    { name: 'Вывести список поездов, по количеству реальных остановок',
+      value: :stops },
+    { name: 'Завершить работу приложения', value: :exit }
+  ].freeze
+
+  def show_menu
+    loop do
+      action = @prompt.select('Выберите действие', MAIN_MENU_CHOICES)
+      break if action == :exit
+    end
+  end
+end

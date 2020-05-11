fe03460a54ff74dec35f19777c13a1d8b53985c4
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2020-05-11 09:30:58 +0300

    Setup static files delivery and add foundation template

--- 2020-05-11-lecture-roda-test-list/test_app.rb
+++ 2020-05-11-lecture-roda-test-list/test_app.rb
@@ -4,7 +4,12 @@ require 'roda'
 
 # The core class of the web application for managing tests
 class TestApp < Roda
+  opts[:root] = __dir__
+  plugin :public
+
   route do |r|
+    r.public
+
     r.root do
       'Hello, world!'
     end
